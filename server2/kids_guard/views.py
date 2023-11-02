8c4a8598a4d72df74f34cd8f3c030ddc6ca97461
woonsan <doldol9080@kookmin.ac.kr>
2023-11-03 01:05:17 +0900

    make api

---
+++ server2/kids_guard/views.py
@@ -0,0 +1,122 @@
+from django.shortcuts import render
+from django.db import connection
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# from .models import Account
+
+# Create your views here.
+@csrf_exempt
+def register(request):
+    # all_records = Account.objects.all()
+    # for i in all_records:
+    #     print(i.id)
+    #     print(i.passward)
+    #     print(i.name)
+    # return HttpResponse("OK", status = 200)
+    # data = json.loads(request.body)
+    # print(data)
+    # print(data['key1'])
+    # print(data['key2'])
+    # return HttpResponse("OK", status = 200)
+    data = json.loads(request.body)
+    id = data['id']
+    password = data['password']
+    name = data['name']
+    zip_code = data['zip_code']
+    phone_number = data['phone_number']
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""insert into account (id, password, name, zip_code, phone_number)
+                            values ('{id}', '{password}', '{name}', '{zip_code}', '{phone_number}') ;""")
+    return HttpResponse("OK", status = 200)
+
+@csrf_exempt
+def login(request):
+    data = json.loads(request.body)
+    id = data['id']
+    password = data['password']
+    print(id)
+    print(password)
+    with connection.cursor() as cursor:
+        cursor.execute(f"""select count(*)
+                            from account
+                            where id = '{id}' and password = '{password}' ;""")
+        login = (cursor.fetchone())[0]
+        print(login)
+    # return HttpResponse("OK", status = 200)
+    if login == 1 :
+        return JsonResponse({'success': True, 'message': '로그인이 성공했습니다.'}, json_dumps_params={'ensure_ascii': False}, content_type = 'application/json; charest=utf-8')
+    else :
+        return JsonResponse({'success': False, 'message': '로그인에 실패했습니다.'}, json_dumps_params={'ensure_ascii': False}, content_type = 'application/json; charest=utf-8')
+
+@csrf_exempt
+def apply_service(request):
+    requestdata = json.loads(request.body)
+    start_location = requestdata['start_location']
+    arrival_location = requestdata['arrival_location']
+    # hospital = requestdata['hospital']
+    # real_time_personal_data = requestdata['real_time_personal_data']
+    child_account_id = requestdata['child_account_id']
+    
+
+    if (start_location == "") or (arrival_location == "") or (child_account_id == ""):
+        return JsonResponse({"success": False, "message": "Invaild Data."}, status = 400)
+
+    else:
+        with connection.cursor() as cursor:
+            cursor.execute(f"""insert into relax_service (start_location, arrival_location, child_name, child_account_id)
+                            values ('{start_location}','{arrival_location}',(SELECT name FROM child WHERE account_id = '{child_account_id}'),'{child_account_id}');""")
+            cursor.execute(f"""insert into status (status, time, relax_service_id)
+                            values ('승인전', NOW() ,(SELECT MAX(id) FROM relax_service));""")
+        return JsonResponse({"success": True, "message": "Service Applied."}, status = 200)
+
+# 관리자가 만들기
+# def modify_state(request):
+#     return JsonResponse({'상태 변경 완료': 1}, content_type ='application/json; charest=utf-8')
+
+@csrf_exempt
+def select_state(request):
+    data = json.loads(request.body)
+    id = data['id']
+    # id = 'abcde'
+    with connection.cursor() as cursor:
+        cursor.execute(f"""select status, time, start_location, arrival_location, hospital
+                            from status
+                            join relax_service on status.relax_service_id = relax_service.id
+                            where relax_service_id = (select id
+                                                        from relax_service
+                                                        where finish = 0)
+                                    and relax_service.child_account_id = '{id}'
+                            order by 2 ;""")
+        state = []
+        for i, v in enumerate(cursor.fetchall()):
+            if i in range(3):
+                state.append({'state': v[0], 'time': v[1],'location': v[2]})
+            if i == 3:
+                state.append({'state': v[0], 'time': v[1],'location': v[4]})
+            if i == 4:
+                state.append({'state': v[0], 'time': v[1],'location': v[3]})
+        print(state)
+
+    return JsonResponse({'states': state}, json_dumps_params={'ensure_ascii': False}, content_type = 'application/json; charest=utf-8')
+
+def select_deposit(request):
+    
+    # requestdata = json.loads(request.body)
+    # account_id = requestdata['account_id']
+
+    account_id = request.GET.get['account_id']
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""SELECT date, change_amount, account_money
+                            FROM account
+                            WRERE account_id = {account_id}
+                            ORDER BY date;""")
+    
+        data = cursor.fetchall()
+        json_data = {"예치금목록": [{"날짜": date, "지출": expenditure, "잔액": balance} for date, expenditure, balance in data]}
+        json_data = json.dumps(json_data, ensure_ascii=False, indent=4)
+        return JsonResponse(json_data, status = 200)
---
+++ server2/server2/urls.py
@@ -0,0 +1,30 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from kids_guard.views import register, login, apply_service, select_state, select_deposit, edit_account_info, delete_account
+
+urlpatterns = [
+    path('register/', register, name = 'registar'), 
+    path('login/', login, name = 'login'),
+    path('apply_service/', apply_service, name='apply_service'), # 출발 장소, 도착 장소를 필수로 입력 받고, 병원 정보, 신청 시점의 아이 정보를 입력으로 받음 -> db에 잘 들어갔으면 response로 신청완료됐다 응답
+    path('select_state/', select_state, name = 'select_state'), #
+    path('select_deposit/', select_deposit, name ='select_dsposit'),
+    path('edit_account_info/', edit_account_info, name = 'edit_account_info'),
+    path('delete_account/', delete_account, name = 'delete_account'),
+
+
+]
