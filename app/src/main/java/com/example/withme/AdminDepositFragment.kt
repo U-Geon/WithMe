3e5b7dfa61456cea4d44fc35be11f6d666413f5f
211_0@naver.com <211_0@naver.com>
2023-11-17 14:42:12 +0900

    주석 제거

--- app/src/main/java/com/example/withme/AdminDepositFragment.kt
+++ app/src/main/java/com/example/withme/AdminDepositFragment.kt
@@ -111,5 +111,5 @@ class AdminDepositFragment : Fragment() {
 //            }
 //        })
 //    }
-    
+
 }
--- app/src/main/java/com/example/withme/AdminDepositManagementActivity.kt
+++ app/src/main/java/com/example/withme/AdminDepositManagementActivity.kt
@@ -30,43 +30,43 @@ class AdminDepositManagementActivity : AppCompatActivity() {
         actionBar?.title = "예치금 관리"
 
         // id, 사용자 이름, 전화번호, 예치금 받아오는 통신 코드
-//        val userId = "csw1234"
-//
-//        if (userId.isNotEmpty()) {
-//            val url = "http://192.168.80.102:8000/select_name_money_count/"
-//
-//            val params = JSONObject()
-//            params.put("id", userId)
-//
-//            val request = JsonObjectRequest(
-//                Request.Method.POST,
-//                url,
-//                params,
-//                Response.Listener { response ->
-//                    try {
-//                        val id = response.getString("id")
-//                        binding.tvUserId.text = "$id"
-//
-//                        val name = response.getString("name")
-//                        val phonenum = response.getString("phone_number")
-//                        binding.tvUserNamePhone.text = "($name, $phonenum)"
-//
-//                        val money = response.getString("money")
-//                        binding.tvCurrentDeposit.text = "현재 예치금: ${money}원"
-//                    } catch (e: JSONException) {
-//                        e.printStackTrace()
-//                    }
-//                },
-//                Response.ErrorListener { error ->
-//                    error.printStackTrace()
-//                    Toast.makeText(this, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
-//                }
-//            )
-//            // Volley 요청을 큐에 추가
-//            Volley.newRequestQueue(this).add(request)
-//        } else {
-//            Toast.makeText(this, "모든 필드를 입력해주세요.", Toast.LENGTH_SHORT).show()
-//        }
-//
+        val userId = "csw1234"
+
+        if (userId.isNotEmpty()) {
+            val url = "http://192.168.80.102:8000/select_name_money_count/"
+
+            val params = JSONObject()
+            params.put("id", userId)
+
+            val request = JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                params,
+                Response.Listener { response ->
+                    try {
+                        val id = response.getString("id")
+                        binding.tvUserId.text = "$id"
+
+                        val name = response.getString("name")
+                        val phonenum = response.getString("phone_number")
+                        binding.tvUserNamePhone.text = "($name, $phonenum)"
+
+                        val money = response.getString("money")
+                        binding.tvCurrentDeposit.text = "현재 예치금: ${money}원"
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                },
+                Response.ErrorListener { error ->
+                    error.printStackTrace()
+                    Toast.makeText(this, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            )
+            // Volley 요청을 큐에 추가
+            Volley.newRequestQueue(this).add(request)
+        } else {
+            Toast.makeText(this, "모든 필드를 입력해주세요.", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
