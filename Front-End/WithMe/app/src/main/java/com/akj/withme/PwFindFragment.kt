65866cde10da2a68e4cbc85425a9420d8cfbdc80
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2023-11-18 19:59:43 +0900

    비밀번호 찾기 POST 요청 구현

--- Front-End/WithMe/app/src/main/java/com/akj/withme/PwFindFragment.kt
+++ Front-End/WithMe/app/src/main/java/com/akj/withme/PwFindFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.akj.withme.databinding.FragmentPwFindBinding
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 class PwFindFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +23,40 @@ class PwFindFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pw_find, container, false)
+        val bind = FragmentPwFindBinding.inflate(layoutInflater)
+
+        bind.pwFindButton.setOnClickListener {
+            if(bind.idText.text.toString() == "" || bind.nameText.text.toString() == "" || bind.phoneText.text.toString() == "") {
+                Toast.makeText(activity, "모든 정보를 입력해 주세요!", Toast.LENGTH_SHORT).show()
+            } else {
+                val url = ""
+                val params = JSONObject()
+                params.put("id", bind.idText.text.toString())
+                params.put("name", bind.nameText.text.toString())
+                params.put("phone", bind.phoneText.text.toString());
+
+                val request = JsonObjectRequest(
+                    Request.Method.POST,
+                    url,
+                    params,
+                    { response -> try {
+
+                    } catch(error: JSONException) {
+                        error.printStackTrace()
+                    }
+                    },
+                    {
+                            error -> {
+                        error.printStackTrace()
+                    }
+                    }
+                )
+
+                Volley.newRequestQueue(activity).add(request)
+            }
+        }
+
+        return bind.root
     }
 
 }
