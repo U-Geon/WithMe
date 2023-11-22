07165f1ba0b042999c3d13b1f5bec08d6e1b7bc9
U-Geon <sksmsfbrjs@naver.com>
2023-11-22 17:39:38 +0900

    최최최최최종

--- app/src/main/java/com/example/withme/administrator/AdminDepositManagementActivity.kt
+++ app/src/main/java/com/example/withme/administrator/AdminDepositManagementActivity.kt
@@ -2,6 +2,8 @@ package com.example.withme.administrator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -14,6 +16,34 @@ import org.json.JSONObject
 
 class AdminDepositManagementActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdminDepositManagementBinding
+    private var moneyInt: Int = 0
+
+    private fun sendMoneyUpdateRequest(updatedMoney: Int) {
+        val sharedPreference = getSharedPreferences("other", 0)  // SharedPreferences 인스턴스
+        val userId = sharedPreference.getString("id", "")
+
+        val url = "http://192.168.80.102:8000/money_deposit/"
+
+        val params = JSONObject()
+        params.put("id", userId)
+        params.put("amount", updatedMoney)
+
+        val request = JsonObjectRequest(
+            Request.Method.POST,
+            url,
+            params,
+            Response.Listener { response ->
+                print("연결 성공")
+            },
+            Response.ErrorListener { error ->
+                error.printStackTrace()
+                Toast.makeText(this, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        // Volley 요청을 큐에 추가
+        Volley.newRequestQueue(this).add(request)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +82,7 @@ class AdminDepositManagementActivity : AppCompatActivity() {
                         binding.tvUserNamePhone.text = "($name, $phonenum)"
 
                         val money = response.getString("money")
+                        moneyInt = money.toInt()
                         binding.tvCurrentDeposit.text = "현재 예치금: ${money}원"
                     } catch (e: JSONException) {
                         e.printStackTrace()
@@ -68,5 +99,35 @@ class AdminDepositManagementActivity : AppCompatActivity() {
             Toast.makeText(this, "모든 필드를 입력해주세요.", Toast.LENGTH_SHORT).show()
         }
 
+        // 예치금 플러스 관련
+        val plusDepositEdit: EditText = findViewById(R.id.et_plus_deposit)
+        val plusDepositButton: Button = findViewById(R.id.btn_plus_deposit)
+        plusDepositButton.setOnClickListener {
+            val depositAmountText: String = plusDepositEdit.text.toString()
+            // 입력된 숫자가 비어있지 않다면 더하도록
+            if (depositAmountText.isNotEmpty()) {
+                val depositAmount: Int = depositAmountText.toInt()
+                moneyInt += depositAmount
+            }
+        }
+
+        // 예치금 마이너스 관련
+        val minusDepositEdit: EditText = findViewById(R.id.et_minus_deposit)
+        val minusDepositButton: Button = findViewById(R.id.btn_minus_deposit)
+        minusDepositButton.setOnClickListener {
+            val depositAmountText: String = minusDepositEdit.text.toString()
+            // 입력된 숫자가 비어있지 않고, 현재 예치금에서 입력된 숫자를 뺐을 때 0 이상일 때만 빼도록
+            if (depositAmountText.isNotEmpty() && moneyInt - depositAmountText.toInt() >= 0) {
+                val depositAmount: Int = depositAmountText.toInt()
+                moneyInt -= depositAmount
+            }
+        }
+
+        // 수정완료 버튼 누르면 서버에 바뀐 예치금 전송
+        val signupFinButton: Button = findViewById(R.id.btn_signup_fin)
+        signupFinButton.setOnClickListener {
+            // 서버에 moneyInt 값을 업데이트
+            sendMoneyUpdateRequest(moneyInt)
+        }
     }
 }
