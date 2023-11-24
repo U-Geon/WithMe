e426d2ba642b83e718b219f5f821366d3cad129b
U-Geon <sksmsfbrjs@naver.com>
2023-11-24 17:01:52 +0900

    fix url

--- app/src/main/java/com/example/withme/administrator/AdminKidInformationActivity.kt
+++ app/src/main/java/com/example/withme/administrator/AdminKidInformationActivity.kt
@@ -35,7 +35,7 @@ class AdminKidInformationActivity : AppCompatActivity() {
          */
 
         // 아이 정보 받아오기
-        val url = resources.getString(R.string.ip) + "/admin_kid_info"
+        val url = "http://15.164.94.136:8000/admin/info" // GET 매핑할 URL
 
         val phoneNumber = binding.phoneNumber
         val kidName = binding.kidName
